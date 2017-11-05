07214cb4d83add5b4ce2744bf33fced7df574793
Perry Pinner <Perry Pinner>
2017-11-05 09:42:23 -0700

    Add project files.

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿var x = 0;
+var s = "";
+
+console.log("hello mr. pinner")
+
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    alert("Buying Item");
+});
+
+var productinfo = $(".product-props li");
